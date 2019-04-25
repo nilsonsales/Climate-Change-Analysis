c5de91424dbf7aa2a57280386b7ed06472a14f2f
Nilson <nilsonsaless@gmail.com>
2019-04-25 00:35:15 -0300

    Update plot.r

--- plot.r
+++ plot.r
@@ -1,5 +1,6 @@
 # Loading ggplot2
 library(ggplot2)
+library(plyr)
 
 # Loading data
 row_data <- read.table("Projects/R/Data Science/Climate Change/GlobalLandTemperaturesByCountry.csv", quote="", header=TRUE, sep=",")
@@ -8,10 +9,13 @@ row_data <- read.table("Projects/R/Data Science/Climate Change/GlobalLandTempera
 temperature <- row_data[complete.cases(row_data),]
 head(temperature)
 
-# Converting data to the specific type
+# Converting dates to the specific date type
 temperature$dt <- as.Date(temperature$dt, format="%Y-%m-%d")
-temperature$dt
 
+
+# Adding new column 'Year'
+Year <- format(temperature$dt, "%Y")
+temperature$Year <- Year
 
 
 # Plotting Brazil average temperature
@@ -26,13 +30,35 @@ ggplot(data = tempBrazil) +
   labs(colour = "Temp")
 
 
+# Getting the average temperature per year
+
+tempBrazil <- aggregate(x = tempBrazil, 
+                        by = list(year = substr(tempBrazil$dt, 1, 4)),
+                        FUN = mean)
+
+ggplot(data = tempBrazil) + 
+  geom_point(mapping =  aes(x = dt, y = AverageTemperature, colour=AverageTemperature)) +
+  geom_smooth(mapping = aes(x = dt, y = AverageTemperature)) +
+  scale_color_gradient(low="blue", high="red") +
+  xlab("Year") + ylab("Temperature") +
+  ggtitle("Brazil Average Temperature 1932-2013") + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(colour = "Temp")
+
+
+
+
 # United States
 tempUS = temperature[ which(temperature$Country=="United States"),]
+
+tempUS <- aggregate(x = tempUS, 
+                        by = list(year = substr(tempUS$dt, 1, 4)),
+                        FUN = mean)
 
 ggplot(data = tempUS) + 
   geom_point(mapping =  aes(x = dt, y = AverageTemperature, colour=AverageTemperature)) +
   geom_smooth(mapping = aes(x = dt, y = AverageTemperature)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  ylim(c(5,12.5))
 
 
 # United Kingdom
