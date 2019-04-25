af9dc777ae5a356f91d622eddeaad03354d97af0
Nilson <nilsonsaless@gmail.com>
2019-04-25 01:19:28 -0300

    Update plot.r

--- plot.r
+++ plot.r
@@ -1,6 +1,5 @@
 # Loading ggplot2
 library(ggplot2)
-library(plyr)
 
 # Loading data
 row_data <- read.table("Projects/R/Data Science/Climate Change/GlobalLandTemperaturesByCountry.csv", quote="", header=TRUE, sep=",")
@@ -18,7 +17,10 @@ Year <- format(temperature$dt, "%Y")
 temperature$Year <- Year
 
 
-## Plotting Brazil average temperature
+
+##### Analysing Brazil, US, UK, Japan and South Africa's temperatures #####
+
+## Brazil
 tempBrazil <- temperature[ which(temperature$Country=="Brazil"),]
 
 ggplot(data = tempBrazil) + 
@@ -29,9 +31,7 @@ ggplot(data = tempBrazil) +
   ggtitle("Brazil Average Temperature 1932-2013") + theme(plot.title = element_text(hjust = 0.5)) +
   labs(colour = "Temp")
 
-
 # Getting the average temperature per year
-
 tempBrazil <- aggregate(x = tempBrazil, 
                         by = list(year = substr(tempBrazil$dt, 1, 4)),
                         FUN = mean)
@@ -45,14 +45,12 @@ ggplot(data = tempBrazil) +
   labs(colour = "Temp")
 
 
-
-
 ## United States
 tempUS = temperature[ which(temperature$Country=="United States"),]
 
 tempUS <- aggregate(x = tempUS, 
-                        by = list(year = substr(tempUS$dt, 1, 4)),
-                        FUN = mean)
+                    by = list(year = substr(tempUS$dt, 1, 4)),
+                    FUN = mean)
 
 # Removing the outliers
 lowerWhisker <- boxplot(tempUS[3])$stats[c(1, 5), ][1]
@@ -64,7 +62,10 @@ tempUS = tempUS[ which(tempUS$AverageTemperature >=  lowerWhisker & tempUS$Avera
 ggplot(data = tempUS) + 
   geom_point(mapping =  aes(x = dt, y = AverageTemperature, colour=AverageTemperature)) +
   geom_smooth(mapping = aes(x = dt, y = AverageTemperature)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  xlab("Year") + ylab("Temperature") +
+  ggtitle("US Average Temperature 1768-2013") + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(colour = "Temp")
 
 
 ## United Kingdom
@@ -74,31 +75,50 @@ tempUK <- aggregate(x = tempUK,
                     by = list(year = substr(tempUK$dt, 1, 4)),
                     FUN = mean)
 
+# Removing the outliers
+lowerWhisker <- boxplot(tempUK[3])$stats[c(1, 5), ][1]
+upperWhisker <- boxplot(tempUK[3])$stats[c(1, 5), ][2]
+
+tempUK = tempUK[ which(tempUK$AverageTemperature >=  lowerWhisker & tempUK$AverageTemperature <= upperWhisker),]
+
+
+
 ggplot(data = tempUK) + 
   geom_point(mapping =  aes(x = dt, y = AverageTemperature, colour=AverageTemperature)) +
   geom_smooth(mapping = aes(x = dt, y = AverageTemperature)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  xlab("Year") + ylab("Temperature") +
+  ggtitle("UK Average Temperature 1743-2013") + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(colour = "Temp")
 
-# Japan
+
+## Japan
 tempJapan = temperature[ which(temperature$Country=="Japan"),]
 
 tempJapan <- aggregate(x = tempJapan, 
-                    by = list(year = substr(tempJapan$dt, 1, 4)),
-                    FUN = mean)
+                       by = list(year = substr(tempJapan$dt, 1, 4)),
+                       FUN = mean)
 
 ggplot(data = tempJapan) + 
   geom_point(mapping =  aes(x = dt, y = AverageTemperature, colour=AverageTemperature)) +
   geom_smooth(mapping = aes(x = dt, y = AverageTemperature)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  xlab("Year") + ylab("Temperature") +
+  ggtitle("Japan Average Temperature 1841-2013") + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(colour = "Temp")
 
-# South Africa
+
+## South Africa
 tempSA = temperature[ which(temperature$Country=="South Africa"),]
 
 tempSA <- aggregate(x = tempSA, 
-                       by = list(year = substr(tempSA$dt, 1, 4)),
-                       FUN = mean)
+                    by = list(year = substr(tempSA$dt, 1, 4)),
+                    FUN = mean)
 
 ggplot(data = tempSA) + 
   geom_point(mapping =  aes(x = dt, y = AverageTemperature, colour=AverageTemperature)) +
   geom_smooth(mapping = aes(x = dt, y = AverageTemperature)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  xlab("Year") + ylab("Temperature") +
+  ggtitle("South Africa Average Temperature 1857-2013") + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(colour = "Temp")
